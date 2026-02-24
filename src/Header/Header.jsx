fa1475f437dd591d234e3f7591fc255043fd3440
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-02-24 16:24:02 +0600

    Initial React protfolio project

---
+++ src/Header/Header.jsx
@@ -0,0 +1,16 @@
+function Header(){
+
+    return (
+
+        <header>
+            <h1>Abdullah Al Sayed</h1>
+            <nav>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </nav>
+        </header>
+    );
+}
+
+export default Header
