981c9c4f61232bc86fdd16bb8bfc17cec5124144
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-02-24 16:46:21 +0600

    Update Header.jsx

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -8,6 +8,7 @@ function Header(){
                 <li><a href="#">Home</a></li>
                 <li><a href="#">About</a></li>
                 <li><a href="#">Contact</a></li>
+                <li><a href="#">blogs</a></li>
             </nav>
         </header>
     );
