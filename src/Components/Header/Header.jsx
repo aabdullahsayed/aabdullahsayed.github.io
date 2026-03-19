0a3d33100627c5620342332dcad9a548a09f56a0
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-03-19 14:37:33 +0600

    Add linkStyle and nav style

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,22 @@
 function Header(){
 
+
+    const linkStyle = {
+        textDecoration: 'none',
+        color: "#333",
+    };
+
     return (
+
+
 
         <header>
             <h1>Abdullah Al Sayed</h1>
-            <nav>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
-                <li><a href="#">blogs</a></li>
+            <nav style ={{backgroundColor:"white", justifyContent:"center", display:"flex",textAlign:"center", gap: "30px"}} >
+                    <a href="#" style={linkStyle}>Home</a>
+                    <a href="#" style={linkStyle}>About</a>
+                    <a href="#" style={linkStyle}>Contact</a>
+                    <a href="#" style={linkStyle}>blogs</a>
             </nav>
         </header>
     );
