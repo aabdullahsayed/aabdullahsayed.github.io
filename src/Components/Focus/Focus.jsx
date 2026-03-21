49ec3c118902ce113f968bd97d579f5b2857e408
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-03-21 19:05:35 +0600

    Add new component

---
+++ src/Components/Focus/Focus.jsx
@@ -0,0 +1,12 @@
+import './Focus.css'
+
+function Focus() {
+
+    return (
+        <>
+        <h3>Research Interests: Deep Learning, Computer Vision</h3>
+        </>
+    );
+}
+
+export default Focus;
