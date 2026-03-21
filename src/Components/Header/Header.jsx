ef8823373e3d5ecd17f5fb54c19dfc08b68bf286
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-03-21 20:00:28 +0600

    Fix url for Homepage

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,7 +4,7 @@ function Header(){
     return (
         <header class="top-header">
             <nav>
-                    <a href="#">Home</a>
+                    <a href="/">Home</a>
                     <a href="#">Projects</a>
                     <a href="#">Contact</a>
                     <a href="#">blogs</a>
