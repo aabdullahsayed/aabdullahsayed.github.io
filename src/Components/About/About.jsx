77e4aeaf7ab9335f0cbd3fb7f4cdc2215559629b
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-03-21 21:23:32 +0600

    Update changes for new package and new hyperlink stype

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -5,7 +5,7 @@ function About(){
             <h1>Abdullah Al Sayed</h1>
             <h4>Full-Stack Developer | C# & JavaScript</h4>
             <div className="about">
-                <p class="intro">Final Year Undergraduate Student in Computer Science and Engineering at <a href="https://iubat.edu" >IUBAT</a>, focused on system design and
+                <p class="intro">Final Year Undergraduate Student in Computer Science and Engineering at <a href="https://iubat.edu" target= "_blank" class="edu" >IUBAT</a>, focused on system design and
                     distributed systems, with experience in C# and JavaScript.
                 </p>
                 <img src="abdullah.png" alt="Abdullah Al Sayed"/>
