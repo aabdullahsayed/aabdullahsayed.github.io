47faea55bbe459e0479098f81a4e61abc1cb8fe0
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-03-21 17:25:26 +0600

    Create new Component about

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,12 @@
+import './About.css'
+function About(){
+    return (
+        <>
+            <h1>Abdullah Al Sayed</h1>
+            <h4>Full-Stack Developer | C# & JavaScript</h4>
+            <p>Final Year Undergraduate Student in Computer Science and Engineering, focused on system design and distributed systems, with experience in C# and JavaScript.</p>
+        </>
+    );
+}
+
+export default About;
